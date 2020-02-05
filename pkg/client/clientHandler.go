dc455f4ef60ce77cecd43d76a4422e09df3cfe79
Ralph Comanne <ralph@comanne.eu>
2020-02-05 18:14:12 +0100

    fix(buffer) - Fixed the buffered reading, use a Read from bufio to read byte by byte

--- pkg/client/clientHandler.go
+++ pkg/client/clientHandler.go
@@ -12,9 +12,9 @@ import (
 // Handle the actual network connection and parsing
 func doHandle(request dom.Request) dom.Response {
 	// Set up connection to remote
-	conn, err := net.Dial(dom.TYPE, dom.HOST+":"+dom.PORT)
-	if err != nil {
-		fmt.Printf("Error while connecting %s\n", err)
+	conn, connErr := net.Dial(dom.TYPE, dom.HOST+":"+dom.PORT)
+	if connErr != nil {
+		fmt.Printf("Error while connecting %s\n", connErr)
 		os.Exit(1)
 	}
 
@@ -27,7 +27,13 @@ func doHandle(request dom.Request) dom.Response {
 	//fmt.Printf("Request String: %s\n", requestString)
 
 	// Write to remote connection
-	fmt.Fprintf(conn, requestString)
+	_, writeErr := conn.Write([]byte(requestString))
+
+	if writeErr != nil {
+		fmt.Printf("Error while sending request.\n%s\n", writeErr)
+	}
+
+	//fmt.Fprintf(conn, requestString)
 
 	// create buffer and copy bytes to it
 	var buf bytes.Buffer
--- pkg/server/serverHandler.go
+++ pkg/server/serverHandler.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"bufio"
 	"bytes"
 	"fmt"
 	dom "github.com/zasei/aftp-server/pkg/domain"
@@ -12,41 +13,58 @@ import (
 const FileDir = "./files"
 
 func HandleRequest(conn net.Conn) {
-	buf := make([]byte, 256)
 
-	//Read the incoming connection into the buffer
-	_, err := conn.Read(buf)
-
-	requestString := strings.Fields(string(buf))
+	// Create a buffered reader for the connection
+	connbuf := bufio.NewReader(conn)
+	// Read first byte and set buffer
+	b, err := connbuf.ReadByte()
 
 	if err != nil {
-		fmt.Println("Error reading", err.Error())
-		handleServerError(conn)
+		fmt.Println("Unable to read from connection.")
 	}
 
-	if len(requestString) < 3 {
-		handleBadRequest(conn, "invalid use of protocol")
-	}
+	if connbuf.Buffered() > 0 {
+		var requestData []byte
+		requestData = append(requestData, b)
+		for connbuf.Buffered() > 0 {
+			// Read byte by byte until empty or error
+			b, err := connbuf.ReadByte()
+			if err == nil {
+				requestData = append(requestData, b)
+			} else {
+				fmt.Println("... Unreadable character ...")
+				fmt.Println(b)
+			}
+		}
+		fullRequest := string(requestData)
+		fmt.Println(fullRequest)
 
-	parsedRequest := dom.ParseRequest(string(bytes.Trim(buf, "\x00")))
+		requestString := strings.Fields(string(requestData))
 
-	parsedRequest.PrintRequest()
+		if len(requestString) < 3 {
+			handleBadRequest(conn, "invalid use of protocol")
+		}
 
-	if !strings.Contains(parsedRequest.Protocol, dom.ProtocolVersion) {
-		handleBadRequest(conn, fmt.Sprintf("unknown protocol %s, wanted %s", parsedRequest.Protocol, dom.ProtocolVersion))
-	}
+		parsedRequest := dom.ParseRequest(string(bytes.Trim(requestData, "\x00")))
 
-	switch parsedRequest.Method {
-	case dom.GET:
-		handleGetRequest(parsedRequest, conn)
-	case dom.LIST:
-		handleListRequest(parsedRequest, conn)
-	case dom.PUT:
-		handlePutRequest(parsedRequest, conn)
-	case dom.DELETE:
-		handleDeleteRequest(parsedRequest, conn)
-	default:
-		handleBadRequest(conn, "unknown/unsupported method: "+parsedRequest.Method)
+		parsedRequest.PrintRequest()
+
+		if !strings.Contains(parsedRequest.Protocol, dom.ProtocolVersion) {
+			handleBadRequest(conn, fmt.Sprintf("unknown protocol %s, wanted %s", parsedRequest.Protocol, dom.ProtocolVersion))
+		}
+
+		switch parsedRequest.Method {
+		case dom.GET:
+			handleGetRequest(parsedRequest, conn)
+		case dom.LIST:
+			handleListRequest(parsedRequest, conn)
+		case dom.PUT:
+			handlePutRequest(parsedRequest, conn)
+		case dom.DELETE:
+			handleDeleteRequest(parsedRequest, conn)
+		default:
+			handleBadRequest(conn, "unknown/unsupported method: "+parsedRequest.Method)
+		}
 	}
 }
 
